ce0e4ac6f6f268f99e759944461b623d703af2b2
Error247 <151540121+Error247@users.noreply.github.com>
2023-12-12 20:30:52 +0000

    updated

---
+++ .github/scripts/update-readme.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+// Get the GitHub username of the latest visitor
+const visitorUsername = process.env.GITHUB_ACTOR;
+
+// Read the current README.md content
+let readmeContent = fs.readFileSync('README.md', 'utf-8');
+
+// Replace a placeholder with the visitor's username
+readmeContent = readmeContent.replace(/<!-- VISITOR_USERNAME -->/g, visitorUsername);
+
+// Write the updated content back to README.md
+fs.writeFileSync('README.md', readmeContent, 'utf-8');
