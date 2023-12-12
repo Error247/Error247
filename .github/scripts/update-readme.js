25be423314beb112c5946b1a380e4aaab23ea357
Error247 <151540121+Error247@users.noreply.github.com>
2023-12-12 20:35:31 +0000

    updated

--- .github/scripts/update-readme.js
+++ .github/scripts/update-readme.js
@@ -1,13 +1,30 @@
 const fs = require('fs');
+const axios = require('axios');
 
-// Get the GitHub username of the latest visitor
-const visitorUsername = process.env.GITHUB_ACTOR || 'Visitor';
+// Get the latest commit information
+async function getLatestCommit() {
+  try {
+    const response = await axios.get('https://api.github.com/repos/:owner/:repo/commits/:ref');
+    return response.data.committer.login;
+  } catch (error) {
+    console.error('Error fetching latest commit information:', error.message);
+    return 'Visitor';
+  }
+}
 
-// Read the current README.md content
-let readmeContent = fs.readFileSync('README.md', 'utf-8');
+// Update README with the latest commit's username
+async function updateReadme() {
+  const visitorUsername = await getLatestCommit();
 
-// Replace a placeholder with the visitor's username
-readmeContent = readmeContent.replace(/<!-- VISITOR_USERNAME -->/g, visitorUsername);
+  // Read the current README.md content
+  let readmeContent = fs.readFileSync('README.md', 'utf-8');
 
-// Write the updated content back to README.md
-fs.writeFileSync('README.md', readmeContent, 'utf-8');
+  // Replace a placeholder with the visitor's username
+  readmeContent = readmeContent.replace(/<!-- VISITOR_USERNAME -->/g, visitorUsername);
+
+  // Write the updated content back to README.md
+  fs.writeFileSync('README.md', readmeContent, 'utf-8');
+}
+
+// Run the updateReadme function
+updateReadme();
